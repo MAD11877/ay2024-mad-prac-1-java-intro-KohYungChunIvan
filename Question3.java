c21d5a97b762764556a4f7675927829215c98a73
KohYungChunIvan <150001174+KohYungChunIvan@users.noreply.github.com>
2024-04-15 14:49:32 +0800

    Update Question3.java

--- Question3.java
+++ Question3.java
@@ -11,7 +11,7 @@ public class Question3
      */
      
     Scanner in = new Scanner(System.in);
-    System.out.print("Please enter an integer: ");
+    System.out.print("");
     int userInt = in.nextInt();
 
     
